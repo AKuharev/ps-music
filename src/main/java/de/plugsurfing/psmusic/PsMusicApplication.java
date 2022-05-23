aef406c210b9a529f5216c50a78c91135cb666fe
Andrei Kukharau <andrei0495@gmail.com>
2022-05-23 19:41:49 +0200

    Init commit

---
+++ src/main/java/de/plugsurfing/psmusic/PsMusicApplication.java
@@ -0,0 +1,13 @@
+package de.plugsurfing.psmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/plugsurfing/psmusic/PsMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package de.plugsurfing.psmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
